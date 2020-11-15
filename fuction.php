1b90e6e795f504914e2da97a81e5865a58912fb6
kato <thippi888@gmail.com>
2020-11-15 15:03:01 +0900

    sub1

---
+++ fuction.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head></head>
+<body>
+<h1>kokoko</h1>
+<?php
+
+$array =['みかん','りんご'];
+
+array_push($array,'ぶどう');
+
+echo'<pre>';
+var_dump($array);
+echo'</pre>';
+
+
+
+
+?>
+
+</body>
+</html>
